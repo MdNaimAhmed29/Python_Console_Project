0c00b1fd0c95f926dfd340b48d62ae29b5fc05b2
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-24 23:14:33 +0600

    updated

---
+++ simpletempconverter.py
@@ -0,0 +1,5 @@
+celsius = input("Enter a temperature in Celsius: ")
+celsius = float(celsius)
+
+fahrenheit = (celsius * 1.8) + 32 #Fahrenheit to Celsius convert formula Celsius multiply with 1.8 or 9/5 and add 32
+print(fahrenheit)
