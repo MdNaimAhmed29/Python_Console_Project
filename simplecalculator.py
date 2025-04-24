3c9656cc953df1a48e3214394409c48bde495e98
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-24 23:00:13 +0600

    updated

---
+++ simplecalculator.py
@@ -0,0 +1,8 @@
+num1 = input("Enter first number: ") #input function always carry str, so need to convert it for print in other data type
+num2 = input("Enter second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+sum = num1 + num2
+print("The sum is: ", sum)
